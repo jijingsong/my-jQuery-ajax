84a59f6bf71c9ed8fa2898436a217fafa7dfd625
jijingsong <787827925@qq.com>
2018-08-29 12:02:22 +0800

    add promise

--- main.js
+++ main.js
@@ -5,35 +5,54 @@ window.jQuery = function (nodeOrSelector) {
     return nodes
 }
 
-window.jQuery.ajax = function({url, method, data, success, error, headers}) {
+window.jQuery.ajax = function ({ url, method, data, success, error, headers }) {
+    return new Promise((resolve, reject) => {
+        let request = new XMLHttpRequest()
 
-    let request = new XMLHttpRequest()
+        request.open(method, url)
 
-    request.open(method, url)
+        for (let key in headers) {
+            let value = headers[key]
+            request.setRequestHeader(key, value)
+        }
 
-    for (let key in headers) {
-        let value = headers[key]
-        request.setRequestHeader(key, value)
-    }
+        request.send(data)
 
-    request.send(data)
-
-    request.onreadystatechange = () => {
-        if (request.readyState === 4) {
-            if (request.status >= 200 && request.status < 300) {
-                let str = request.responseText
-                let obj = JSON.parse(str)
-                success.call(undefined, request.responseText)
-            } else {
-                error.call(undefined, request)
+        request.onreadystatechange = () => {
+            if (request.readyState === 4) {
+                if (request.status >= 200 && request.status < 300) {
+                    let str = request.responseText
+                    let obj = JSON.parse(str)
+                    resolve.call(undefined, request.responseText)
+                } else {
+                    reject.call(undefined, request)
+                }
             }
         }
-    }
+    })
+
 }
 
 window.$ = window.jQuery
 
 btn.addEventListener('click', () => {
+    
+    $.ajax({
+        url: '/xxx',
+        method: 'post',
+        data: 'haha',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'jjs': '18'
+        }
+    })
+        .then((text) => {
+            console.log(text)
+        }, (request) => {
+            console.log(request)
+        })
+
+    // 原生JS版本
     // let request = new XMLHttpRequest()
 
     // request.open('get', '/xxx')
@@ -53,21 +72,26 @@ btn.addEventListener('click', () => {
     //         }
     //     }
     // }
-    $.ajax({
-        url: '/xxx',
-        method:'post',
-        data: 'haha',
-        success: (x) => {
-            console.log(x)
-        },
-        error: (x) => {
-            console.log(x)
-        },
-        headers: {
-            'content-type': 'application/x-www-form-urlencoded',
-            'jjs': '18'
-        }
-    })
+
+
+    // Jquery版本
+    // $.ajax({
+    //     url: "/xxx",
+    //     method: "POST",
+    //     data: {
+    //         'jjs': '18'
+    //     },
+    //     dataType: "html",
+    // })
+    //     .then((responseText) => {
+    //         console.log(responseText)
+    //         return responseText
+    //     }, (error) => {
+    //         console.log(error)
+    //     })
+    //     .then((x) => {
+    //         console.log(x)
+    //     })
 })
 
 
